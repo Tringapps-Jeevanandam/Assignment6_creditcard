14f7736d1ea4a88acab94ff3f68babfca660da07
Tringapps-Jeevanandam <123439486+Tringapps-Jeevanandam@users.noreply.github.com>
2023-01-30 11:34:19 +0530

    Update creditcard.java

--- src/main/java/je/creditcard.java
+++ src/main/java/je/creditcard.java
@@ -1,7 +1,8 @@
 package je;
 
 import java.util.*;
-class CREDIT implements Cloneable{
+import java.util.logging.Logger;
+class credit implements Cloneable{
     String cardHolderName;
     long cardNumber;
     String expDate;
@@ -20,36 +21,32 @@ class CREDIT implements Cloneable{
         }  
 
 }
-class CREDITCARD{
+class creditcard{
+    public static final Logger Log = Logger.getLogger("InfoLogging");
     public static void main(String[] args) throws CloneNotSupportedException {
         String cardHolderName;
         long cardNumber;
         String expDate;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Credit Card details:\nCard Holder Name: ");
+        Log.info("Enter Credit Card details:\nCard Holder Name: ");
         cardHolderName = sc.nextLine();
-        System.out.println("Card Number:");
+        Log.info("Card Number:");
         cardNumber = sc.nextLong();
-        System.out.println("Exp Date:");
+        Log.info("Exp Date:");
         expDate = sc.next();
-
         credit cr = new credit(cardHolderName, cardNumber, expDate);
         credit cr2 = (credit)cr.clone();
-        System.out.println("New card Number: ");
+        Log.info("New card Number: ");
         long newCardNumber = sc.nextLong();
         if(cr.equals(newCardNumber)==false){
-            System.out.println("Card number updated");
+            Log.info("Card number updated");
             cr2.cardNumber = newCardNumber;
         }
         else{
-            System.out.println("Card Numbersk are same");
+            Log.info("Card Numbersk are same");
         }
-        System.out.println("ORIGINAL CARD DETAILS: Card Holder Name: "+cr.cardHolderName+" Card Number:"+cr.cardNumber+" Exp Date: "+cr.expDate);
-        System.out.println("AFTER CLONING : Card Holder Name: "+cr2.cardHolderName+" Card Number:"+cr2.cardNumber+" Exp Date: "+cr2.expDate);
-
-
-
-
+        Log.info("ORIGINAL CARD DETAILS: Card Holder Name: "+cr.cardHolderName+" Card Number:"+cr.cardNumber+" Exp Date: "+cr.expDate);
+        Log.info("AFTER CLONING : Card Holder Name: "+cr2.cardHolderName+" Card Number:"+cr2.cardNumber+" Exp Date: "+cr2.expDate);
 
     }
 }
